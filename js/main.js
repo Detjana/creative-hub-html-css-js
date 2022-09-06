3e09798ca620500f4b7a7dcedbd88065f0ddcee4
detjanakastratifrakton <detjana.kastati.frakton@doodle.com>
2022-09-06 19:59:21 +0200

    working on calculator

--- js/main.js
+++ js/main.js
@@ -1,70 +1,29 @@
-let burgerMenu = document.getElementById("header__burger");
-burgerMenu.addEventListener("click", addClassToBody);
+// let burgerMenu = document.getElementById("header__burger");
+// burgerMenu.addEventListener("click", addClassToBody);
 
-function addClassToBody() {
-    document.querySelector("body").classList.toggle("menu-open");
+// function addClassToBody() {
+//     document.querySelector("body").classList.toggle("menu-open");
+// }
+
+let operators = document.getElementsByClassName("operator");
+
+console.log(operators);
+console.log(operators[0]);
+
+// operators[0].addEventListener
+
+// for(let i = 0; i < operators.length; i++) {
+//     console.log(i);
+//     console.log(operators[i])
+//     operators[i].addEventListener("click", function showResult() {
+//         console.log(this.operator);
+//     });
+// }
+
+function calculate(event) {
+    let firstInp = document.getElementById("firstInp").valueAsNumber;
+    let lastInp = document.getElementById("lastInp").valueAsNumber;
+    console.log(firstInp);
+    console.log(lastInp);
+    console.log(event.value)
 }
-
-
-let firstVariable = 2;
-let secondVariable = "2"
-
-// console.log(firstVariable);
-// console.log(secondVariable, " test param ", 2, " ",secondVariable);
-
-// console.log(firstVariable == secondVariable);
-// console.log(firstVariable === secondVariable);
-
-let firstBoolean = true;
-let secondBoolean = false;
-
-// console.log(firstBoolean)
-
-// console.log(!firstBoolean)
-
-// console.log(firstBoolean != secondBoolean);
-
-let iceCream = "vanilla";
-if(iceCream == "chocolate") {
-    // alert("Yeyy I love chocolate ice cream");
-} else {
-    // alert("But chocolate is my afv!");
-}
-
-function multiply(num1, num2) {
-    return num1 === num2;
-}
-
-// console.log(multiply(2, 3));
-
-// console.log(multiply(3, "3"));
-
-// document.getElementById("input").addEventListener("change", inputChanged);
-
-// function inputChanged() {
-//     console.log("input changed");
-// }
-
-// document.getElementById("input").addEventListener("mouseover", mouseoverFunction);
-
-// function mouseoverFunction() {
-//     console.log("mouse is over input");
-// }
-
-// document.getElementById("input").addEventListener("mouseout", mouseoutFunction);
-
-// function mouseoutFunction() {
-//     console.log("mouse is out of input");
-// }
-
-// window.addEventListener("load", onLoadFunction);
-
-// function onLoadFunction() {
-//     console.log("page is loaded");
-// }
-
-// document.getElementById("input").addEventListener("keydown", onkeydownFunction);
-
-// function onkeydownFunction() {
-//     console.log('input key added');
-// }
