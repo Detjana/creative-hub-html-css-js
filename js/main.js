af19a32b852a7bd2a844c503021cf6d1c8fb104f
detjanakastratifrakton <detjana.kastati.frakton@doodle.com>
2022-09-13 18:37:40 +0200

    finalized simple calculator

--- js/main.js
+++ js/main.js
@@ -1,29 +1,25 @@
-// let burgerMenu = document.getElementById("header__burger");
-// burgerMenu.addEventListener("click", addClassToBody);
+let burgerMenu = document.getElementById("header__burger");
+if(burgerMenu) {
+    burgerMenu.addEventListener("click", addClassToBody);
+}
 
-// function addClassToBody() {
-//     document.querySelector("body").classList.toggle("menu-open");
-// }
-
-let operators = document.getElementsByClassName("operator");
-
-console.log(operators);
-console.log(operators[0]);
-
-// operators[0].addEventListener
-
-// for(let i = 0; i < operators.length; i++) {
-//     console.log(i);
-//     console.log(operators[i])
-//     operators[i].addEventListener("click", function showResult() {
-//         console.log(this.operator);
-//     });
-// }
+function addClassToBody() {
+    document.querySelector("body").classList.toggle("menu-open");
+}
 
 function calculate(event) {
     let firstInp = document.getElementById("firstInp").valueAsNumber;
     let lastInp = document.getElementById("lastInp").valueAsNumber;
-    console.log(firstInp);
-    console.log(lastInp);
-    console.log(event.value)
+    let result;
+
+    if(event.value === "+") {
+        result = firstInp + lastInp;
+    } else if(event.value === "-") {
+        result = firstInp - lastInp;
+    } else if(event.value === "*") {
+        result = firstInp * lastInp;
+    } else if(event.value === "/") {
+        result = firstInp / lastInp;
+    }
+    document.getElementById("result").innerHTML = result;
 }
